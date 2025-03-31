c37b54c1d1a418afa2f9fd81a608ab4a3a37c1fb
Peter Foltín <system15@gmail.com>
2025-03-31 15:14:53 +0200

    oprava import DB

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,8 +1,8 @@
 # scripts/create_admin.py
 
 from sqlalchemy.orm import Session
-#from app.database import engine, User, get_password_hash
-from database import engine, User, get_password_hash
+from app.database import engine, User, get_password_hash
+#from database import engine, User, get_password_hash
 
 def create_admin(email: str, password: str):
     db = Session(bind=engine)
